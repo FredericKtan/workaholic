99919eafd1aeb397f1b672da69b1dcab2acfff87
fredericmamath <fredericmamath@gmail.com>
2017-11-17 00:16:43 +0100

    (config) added eslint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-import './App.css';
+
+const passwordIsValid = (password, pattern) => {
+  return true; // fonction de comparaison de pattern
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      password: '',
+      confirmPassword: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ 
+      [event.key]: event.value
+      passwordIsValid = passwordIsValid(password),
+      confirmPasswordIsValid = passwordIsValid(confirmPassword);
+    });
+  }
+
   render() {
     return (
-      <div className="app">
-        <span style={{color: "grey", fontSize: "135px"}}>&#x2B22;Workaholic</span>
-      </div>
+      <form>
+        <input type="text" placeholder="passsword" onChange={ this.handleChange } />
+        <input type="text" placeholder="confirmPassword" onChange={ this.handleChange } />
+      </form>
     );
-  }
+  } 
+  
 }
 
 export default App;
