65c7959f80224d0e2cb3cfd8ae3d2150622e4f58
fredericmamath <fredericmamath@gmail.com>
2017-11-18 00:51:35 +0100

    :wrench: Added rules to linter

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    rules: {
+     "linebreak-style": 0,
+     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    }
 };
