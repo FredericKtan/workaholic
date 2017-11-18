bd33f913d6f4f59dffe4f14c236b2a3e5a95475f
fredericmamath <fredericmamath@gmail.com>
2017-11-18 01:35:39 +0100

    :art: Added basic interface

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './App.css';
 
 const App = () => (
   <div className="app">
-    <p>Workaholic</p>
+    <header><h1>Workaholic</h1></header>
+    <div>
+      <ul>
+        <li>Message 1</li>
+        <li>Message 2</li>
+        <li>Message 3</li>
+        <li>Message 4</li>
+        <li>Message 5</li>
+      </ul>
+      <form>
+        <input type="text" placeholder="message" />
+        <input type="submit" value="send" />
+      </form>
+    </div>
   </div>
 );
 
