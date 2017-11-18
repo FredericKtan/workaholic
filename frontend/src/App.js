e57622a2c23a2aea2fc1bfe39071f03bcaff5641
fredericmamath <fredericmamath@gmail.com>
2017-11-18 01:51:13 +0100

    :art: Splitted app into 3 sections

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,20 @@ import './App.css';
 const App = () => (
   <div className="app">
     <header className="header"><h1>Workaholic</h1></header>
-    <div>
-      <ul>
-        <li>Message 1</li>
-        <li>Message 2</li>
-        <li>Message 3</li>
-        <li>Message 4</li>
-        <li>Message 5</li>
-      </ul>
+    <div className="layout-column-reversed">
       <form>
         <input type="text" placeholder="message" />
         <input type="submit" value="send" />
       </form>
+      <div className="layout-column">
+        <ul>
+          <li>Message 1</li>
+          <li>Message 2</li>
+          <li>Message 3</li>
+          <li>Message 4</li>
+          <li>Message 5</li>
+        </ul>
+      </div>
     </div>
   </div>
 );
