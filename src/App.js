c58f5aa084db76b56ac1f497ba9c9a24719aa047
fredericmamath <fredericmamath@gmail.com>
2017-11-18 02:38:32 +0100

    :art: Added vertical layout component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import Layout from './components/Layout';
+
 const App = () => (
   <div className="app">
     <header className="header"><h1>Workaholic</h1></header>
-    <div className="layout-column-reversed">
+    <Layout reversed>
       <form>
         <input type="text" placeholder="message" />
         <input type="submit" value="send" />
       </form>
-      <div className="layout-column">
+      <Layout>
         <ul>
           <li>Message 1</li>
           <li>Message 2</li>
@@ -17,8 +19,8 @@ const App = () => (
           <li>Message 4</li>
           <li>Message 5</li>
         </ul>
-      </div>
-    </div>
+      </Layout>
+    </Layout>
   </div>
 );
 
