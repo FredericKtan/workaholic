3b29c46c76197ae2acd33f40349d1cb60120792d
fredericmamath <fredericmamath@gmail.com>
2017-11-22 23:43:02 +0100

    :sparkles: Added a store and a reducer

--- src/App.js
+++ src/App.js
@@ -6,27 +6,29 @@ import UIHorizontalLayout from './components/UI/UIHorizontalLayout';
 
 import Message from './components/Message';
 
-const App = () => (
-  <div className="app">
-    <header className="header"><span>FM</span><h1>Workaholic</h1></header>
-    <UIVerticalLayout reversed>
-      <form>
-        <UIHorizontalLayout>
-          <input type="text" placeholder="message" />
-          <input type="submit" value="send" />
-        </UIHorizontalLayout>
-      </form>
-      <UIVerticalLayout>
-        <ul>
-          <li><Message text="Message 1" /></li>
-          <li><Message text="Message 2" /></li>
-          <li><Message text="Message 3" /></li>
-          <li><Message text="Message 4" /></li>
-          <li><Message text="Message 5" /></li>
-        </ul>
+const App = ({ store }) => {
+  return (
+    <div className="app">
+      <header className="header"><span>FM</span><h1>Workaholic</h1></header>
+      <UIVerticalLayout reversed>
+        <form>
+          <UIHorizontalLayout>
+            <input type="text" placeholder="message" />
+            <input type="submit" value="send" />
+          </UIHorizontalLayout>
+        </form>
+        <UIVerticalLayout>
+          <ul>
+            <li><Message text="Message 1" /></li>
+            <li><Message text="Message 2" /></li>
+            <li><Message text="Message 3" /></li>
+            <li><Message text="Message 4" /></li>
+            <li><Message text="Message 5" /></li>
+          </ul>
+        </UIVerticalLayout>
       </UIVerticalLayout>
-    </UIVerticalLayout>
-  </div>
-);
-
+    </div>
+  );
+}
+  
 export default App;
---
+++ src/reducers/messageReducer.js
@@ -0,0 +1,17 @@
+// Reducer
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'CREATE_MESSAGE':
+      return state;
+    case 'READ_MESSAGE':
+      return state;
+    case 'UPDATE_MESSAGE':
+      return state;
+    case 'DELETE_MESSAGE':
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default messageReducer;
