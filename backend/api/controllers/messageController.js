fc542d393ef29fa728deae31ad5ebe3a19c3b0a7
TANG Thanh Lam <tanglam77@gmail.com>
2017-11-24 00:29:34 +0100

    promises update

--- backend/api/controllers/messageController.js
+++ backend/api/controllers/messageController.js
@@ -3,12 +3,13 @@ var Message = mongoose.model('Messages');
 
 //GET ALL Messages
 exports.readAll = function(req, res){
-    Message.find({}, (err, messages) =>{
-        if(err){
-            res.status(500).send({message : "Some error occured while retrieving messages."});
-        }
+    Message.find({})
+      .catch((err) => {
+        res.status(500).send({message : "Some error occured while retrieving messages."});
+      })
+      .then((messages) =>{
         res.status(200).send(messages);
-    });
+      });
     console.log("API called GET : Message / GET ALL");
 }
 
@@ -17,12 +18,13 @@ exports.read = function(req, res){
     if(!req.params.messageId){
         res.status(400).send({message : "messageId can not be empty"});
     }    
-    Message.findById(req.params.messageId, function(err, message){
-      if(err) {
+    Message.findById(req.params.messageId)
+      .catch((err) =>{
         res.status(500).send({message : "Could not retrieve message with id "+req.params.messageId});
-      }
-      res.status(200).send(message);
-    });
+      })
+      .then((message) =>{
+        res.status(200).send(message);
+      });
     console.log("API called GET : Message / GET by ID");
 };
 
@@ -52,10 +54,11 @@ exports.update = function(req, res){
     Message.findOneAndUpdate({ _id : req.params.messageId}, {
         text : req.body.text,
         editedAt : Date.now()
-    }, {new : true}, function(err, message){
-      if(err){
-        res.status(500).send({message: "Could not update Message with id " + req.params.messageId});
-      }
+    }, {new : true})
+    .catch((err) =>{
+      res.status(500).send({message: "Could not update Message with id " + req.params.messageId});
+    })
+    .then((message) =>{
       res.status(200).send(message);
     });
     console.log("API called POST : Message / POST");
@@ -66,10 +69,11 @@ exports.delete = function(req, res){
     if(!req.params.messageId){
         res.status(400).send({message : "messageId can not be empty"});
     }     
-    Message.remove({ _id : res.params.messageId}, function(err, message){
-      if(err){
-        res.status(500).send({message: "Could not update Message with id " + req.params.messageId});
-      }
+    Message.remove({ _id : res.params.messageId})
+    .catch((err) =>{
+      res.status(500).send({message: "Could not update Message with id " + req.params.messageId}); 
+    })
+    .then(() =>{
       res.status(200).send({message: 'This Message is deleted'});
     });
     console.log("API called DELETE : Message / DELETE");
