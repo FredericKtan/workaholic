44255cf47f00c68e3cbae6219138de834a6d68bf
fredericmamath <fredericmamath@gmail.com>
2017-11-26 23:57:30 +0100

    :art: Added fluid sidebar animation

--- src/App.js
+++ src/App.js
@@ -13,9 +13,14 @@ import DeveloperIndicator from './components/Developer/DeveloperIndicator';
 // Component
 import UIHorizontalLayout from './components/UI/UIHorizontalLayout';
 import UIVerticalLayout from './components/UI/UIVerticalLayout';
-import UISideBar from './components/UI/UISideBar';
 
 import Message from './components/Message';
+
+const getElements = items => (
+  items.map((item, key) => (
+    <li key={item}>Element {key}</li>
+  ))
+);
 
 class App extends Component {
   constructor(props) {
@@ -26,17 +31,12 @@ class App extends Component {
     };
 
     this.handleOnClick = this.handleOnClick.bind(this);
-    this.toggle = this.toggle.bind(this);
   }
 
   handleOnClick() {
-    this.props.store.dispatch(createMessage('Hello world'));
-  }
+    // this.props.store.dispatch(createMessage('Hello world'));
 
-  toggle() {
-    console.log(this.sidebar);
-
-    TweenLite.to(this.sidebar, 1, {
+    TweenLite.to(this.sideBar, 1, {
       width: this.state.sideBarIsOpen ? '0px' : '500px',
       opacity: this.state.sideBarIsOpen ? '0' : '1',
       onComplete: () => {},
@@ -55,41 +55,51 @@ class App extends Component {
         height: '100%',
         width: '500px',
         opacity: '1',
+        overflowY: 'hidden',
       },
     };
 
     return (
       <div className="app">
-        <UIHorizontalLayout>
-          <UISideBar
-            width="500px"
-          />
-          <UIVerticalLayout>
-            <header className="header">
-              <span>FM</span>
-              <h1>Workaholic</h1>
-              <button onClick={this.toggle}>Press me !</button>
-            </header>
-            <UIVerticalLayout reversed>
-              <form>
-                <UIHorizontalLayout>
-                  <input type="text" placeholder="message" />
-                  <input type="submit" value="send" />
-                </UIHorizontalLayout>
-              </form>
-              <UIVerticalLayout>
-                <ul>
-                  <li><Message text="Message 1" /></li>
-                  <li><Message text="Message 2" /></li>
-                  <li><Message text="Message 3" /></li>
-                  <li><Message text="Message 4" /></li>
-                  <li><Message text="Message 5" /></li>
-                  <li><Message text={this.props.store.getState().message} /></li>
-                </ul>
+        <UIVerticalLayout>
+          <header className="header">
+            <span>FM</span>
+            <h1>Workaholic</h1>
+            <button onClick={this.handleOnClick}>Press me !</button>
+          </header>
+          <UIHorizontalLayout>
+            <div
+              ref={(ref) => { this.sideBar = ref; }}
+              style={{ ...styles.sideBar }}
+            >
+              <h2>Menu</h2>
+              <hr />
+              <ul>
+                {getElements(['a', 'b', 'c'])}
+              </ul>
+            </div>
+            <UIVerticalLayout>
+              <UIVerticalLayout reversed>
+                <form>
+                  <UIHorizontalLayout>
+                    <input type="text" placeholder="message" />
+                    <input type="submit" value="send" />
+                  </UIHorizontalLayout>
+                </form>
+                <UIVerticalLayout>
+                  <ul>
+                    <li><Message text="Message 1" /></li>
+                    <li><Message text="Message 2" /></li>
+                    <li><Message text="Message 3" /></li>
+                    <li><Message text="Message 4" /></li>
+                    <li><Message text="Message 5" /></li>
+                    <li><Message text={this.props.store.getState().message} /></li>
+                  </ul>
+                </UIVerticalLayout>
               </UIVerticalLayout>
             </UIVerticalLayout>
-          </UIVerticalLayout>
-        </UIHorizontalLayout>
+          </UIHorizontalLayout>
+        </UIVerticalLayout>
         { process.env.NODE_ENV ? <DeveloperIndicator /> : null }
       </div>
     );
